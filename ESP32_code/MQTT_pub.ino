31f3749619f93af72c4d2477a363bd283b50c8d2
mhilmir <rusydiansyahhilmi@gmail.com>
2023-12-11 15:47:35 +0700

    tambah kode arduino

---
+++ ESP32_code/MQTT_pub.ino
@@ -0,0 +1,63 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+uint8_t temprature_sens_read();
+#ifdef __cplusplus
+}
+#endif
+uint8_t temprature_sens_read();
+
+// WiFi
+const char *ssid = "keripikUdang"; // Enter your WiFi name
+const char *password = "keripikMbote"; // Enter WiFi password
+
+// MQTT Broker
+const char *mqtt_broker = "broker.emqx.io";
+const char *topic = "esp32/data_dht";
+const char *mqtt_username = "emqx";
+const char *mqtt_password = "public";
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  // Set software serial baud to 115200;
+  Serial.begin(115200);
+  // connecting to a WiFi network
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+
+  //connecting to a mqtt broker
+  client.setServer(mqtt_broker, mqtt_port);
+
+  while (!client.connected()) {
+    String client_id = "esp32testClient-";
+    client_id += String(WiFi.macAddress());
+    Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("Public emqx mqtt broker connected");
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+}
+
+void loop() {
+  // float temp = (temprature_sens_read() - 32) / 1.8;
+  float temp = random(20, 40);
+  char temps[10];
+  sprintf(temps,"%3.2f", temp);
+  client.publish(topic, temps);
+  client.loop();
+  delay(1000);
+}
---
+++ ESP32_code/post_HTTP.ino
@@ -0,0 +1,59 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+uint8_t temprature_sens_read();
+#ifdef __cplusplus
+}
+#endif
+uint8_t temprature_sens_read();
+
+//Masukan ssid dan password dari router
+const char* ssid = "keripikUdang";
+const char* password = "keripikMbote";
+
+//Alamat IP server dengan portnya
+unsigned long lastTime = 0;
+unsigned long timerDelay = 1000;
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    //cek koneksi
+    if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+      float temp = (temprature_sens_read() - 32) / 1.8;
+      char serverName[100];
+      sprintf(serverName,"http://192.168.43.95:5000/esp?temperature=%3.2f", temp);
+      http.begin(client, serverName);
+
+      // kirimkan HTTP POST
+      int httpResponseCode = http.POST();
+        
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
